5b894897d45caaf3e3095d89415cb493e985ae90
picom <picom@picom.ru>
2016-09-12 17:05:26 +0300

    deleting files

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=telegind_db',
-    'username' => 'telegind',
-    'password' => 'FvGsvxAN',
-    'charset' => 'utf8',
-];
